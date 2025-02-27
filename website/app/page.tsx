e419fd9414cab7f42e64702ec1b0e5a744e3c2ce
Manthan Sharma <dev.manthansharma@gmail.com>
2025-02-28 04:14:20 +0530

    [Updated] - Website Code to handle V1 password generation

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -1,101 +1,140 @@
-import Image from "next/image";
+"use client";
 
+import { FormEvent, useState, useEffect } from "react";
+import core from "@dev-manthan-sharma/paw-ma--core";
+
+/**
+ * Renders the Home Page
+ * @returns - Home Page
+ */
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [url, setUrl] = useState("");
+  const [masterPassword, setMasterPassword] = useState("");
+  const [domainFound, setDomainFound] = useState("");
+  const [generatedPassword, setGeneratedPassword] = useState("");
+  const [error, setError] = useState("");
+  const [isCopied, setIsCopied] = useState(false);
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+  useEffect(() => {
+    // If either Url or Master Password are empty clear derived states
+    if (!url || !masterPassword) {
+      setDomainFound("");
+      setGeneratedPassword("");
+      setError("");
+    }
+
+    // If both Url and Master Password are not empty run v1 code and update derived states
+    if (url || masterPassword) {
+      const v1 = core.v1(url, masterPassword);
+      setDomainFound(v1.domain || "");
+      setGeneratedPassword(v1.generatedPassword || "");
+      setError(v1.error || "");
+    }
+  }, [url, masterPassword]);
+
+  // Function to handle form submission
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    //Copy Generated Password
+    setIsCopied(true)
+    navigator.clipboard.writeText(generatedPassword);
+    setTimeout(() => {
+      setIsCopied(false);
+    }, 3000)
+  };
+
+  return (
+    <div className="grid grid-cols-12">
+      {/* Main Content */}
+      <main className="col-span-12">
+        {/* Hero Section */}
+        <section className="relative min-h-screen flex flex-col items-start justify-center p-8 sm:p-12 md:p-20 md:pb-2 bg-black">
+          <div className="flex-auto grid grid-cols-1 gap-4 lg:grid-cols-4 lg:gap-8 w-full">
+            <div className="lg:col-span-2 text-white self-center">
+              <h1 className="text-4xl font-bold">Paw-Ma</h1>
+              <p className="text-xl pt-10">
+                A Simple Password Manager
+                <br />
+                <br />
+                Use one Master Password to create multiple passwords
+                <br />
+                So you don’t have to remember all the passwords for all the
+                websites or just use one password for all the websites
+                <br />
+                <br />
+                Just copy the link of the website you want to create password
+                for and paste it here, then type your master password, and it
+                will create a password just for you
+              </p>
+            </div>
+            <div className="rounded-3xl lg:col-span-2 bg-gray-100 drop-shadow-md shadow-lg shadow-white/50 flex flex-col items-start justify-center self-center">
+              <form className="w-full p-8" onSubmit={handleSubmit}>
+                <input
+                  type="text"
+                  name="url"
+                  value={url}
+                  onChange={(e) => {
+                    setUrl(e.target.value.trim());
+                  }}
+                  placeholder="URL"
+                  required
+                  autoComplete="off"
+                  className="w-full p-2 border-2 border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-400 focus:border-transparent"
+                />
+                <input
+                  type="password"
+                  name="master"
+                  value={masterPassword}
+                  onChange={(e) => {
+                    setMasterPassword(e.target.value.trim());
+                  }}
+                  placeholder="Master Password"
+                  required
+                  autoComplete="off"
+                  className="w-full p-2 mt-4 border-2 border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-400 focus:border-transparent"
+                />
+                <input
+                  type="text"
+                  name="domain"
+                  value={domainFound}
+                  placeholder="Domain"
+                  disabled
+                  autoComplete="off"
+                  className="w-full p-2 mt-8 border-2 bg-gray-200 border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-400 focus:border-transparent"
+                />
+                <input
+                  type="text"
+                  name="password"
+                  value={generatedPassword}
+                  placeholder="Password"
+                  disabled
+                  autoComplete="off"
+                  className="w-full p-2 mt-4 border-2 bg-gray-200 border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-400 focus:border-transparent"
+                />
+
+                <button
+                  type="submit"
+                  disabled={error.length !== 0}
+                  className={`w-full mt-8 rounded-md ${
+                    error.length
+                      ? "bg-red-800 hover:bg-red-700"
+                      : "bg-gray-900 hover:bg-gray-600"
+                  } px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-400`}
+                >
+                  {error || (isCopied ? "Copied" : "Copy")}
+                </button>
+              </form>
+            </div>
+          </div>
+
+          <div className="mx-auto max-w-5xl mt-4">
+            <p className="text-center text-gray-400">
+              Made with ❤️ by Manthan Sharma
+            </p>
+          </div>
+        </section>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }

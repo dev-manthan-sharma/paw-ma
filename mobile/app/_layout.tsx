d39851414ed92b74f8e5db2c8b89e80914c96290
Manthan Sharma <dev.manthansharma@gmail.com>
2025-02-28 21:30:31 +0530

    [Added] - Mobile App UI Code

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,5 +1,17 @@
+import { DefaultTheme, ThemeProvider } from "@react-navigation/native";
 import { Stack } from "expo-router";
+import { StatusBar } from "react-native";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ThemeProvider value={DefaultTheme}>
+      <Stack
+        screenOptions={{
+          statusBarHidden: true,
+          headerShown: false,
+        }}
+      />
+      <StatusBar hidden={true} barStyle={"default"} />
+    </ThemeProvider>
+  );
 }
--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Pressable, Text, TextInput, View } from "react-native";
 
 export default function Index() {
   return (
@@ -7,9 +7,95 @@ export default function Index() {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        backgroundColor: "#000",
+        padding: 16,
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <View style={{ justifyContent: "center", alignItems: "center" }}>
+        <Text style={{ color: "#fff", fontSize: 60, fontWeight: "900" }}>
+          Paw-Ma
+        </Text>
+        <Text style={{ color: "#fff", fontSize: 20 }}>
+          A Simple Password Manager
+        </Text>
+      </View>
+
+      <View
+        style={{
+          marginTop: 36,
+          backgroundColor: "#f3f4f6",
+          borderRadius: 16,
+          paddingHorizontal: 24,
+          paddingVertical: 36,
+          width: "90%",
+          overflow: "hidden",
+        }}
+      >
+        <TextInput
+          placeholder="URL"
+          style={{
+            fontSize: 20,
+            padding: 12,
+            borderRadius: 16,
+            borderColor: "#d1d5dc",
+            borderWidth: 1
+          }}
+          multiline={true}
+        />
+        <TextInput
+          placeholder="Master Password"
+          style={{
+            fontSize: 20,
+            marginTop: 24,
+            padding: 12,
+            borderRadius: 16,
+            borderColor: "#d1d5dc",
+            borderWidth: 1
+          }}
+          multiline={true}
+        />
+        <TextInput
+          placeholder="Domain"
+          style={{
+            fontSize: 20,
+            backgroundColor: "#e5e7eb",
+            marginTop: 36,
+            padding: 12,
+            borderRadius: 16,
+            borderColor: "#d1d5dc",
+            borderWidth: 1
+          }}
+          editable={false}
+          multiline={true}
+        />
+        <TextInput
+          placeholder="Password"
+          style={{
+            fontSize: 20,
+            backgroundColor: "#e5e7eb",
+            marginTop: 24,
+            padding: 12,
+            borderRadius: 16,
+            borderColor: "#d1d5dc",
+            borderWidth: 1
+          }}
+          editable={false}
+          multiline={true}
+        />
+        <Pressable style={{
+          backgroundColor: "#101828",
+          marginTop: 36,
+          padding: 12,
+          borderRadius: 16,
+          overflow: "hidden",
+          justifyContent: "center",
+          alignItems: "center"
+        }} android_ripple={{color: "#4a5565", borderless: false, foreground: true}}>
+          <Text style={{ color: "#fff", fontSize: 20 }}>
+            Copy
+          </Text>
+        </Pressable>
+      </View>
     </View>
   );
 }
